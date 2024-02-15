b5d436c4c19d5bbb35d007424ad64cf5466bf78e
Adjei Rapheal <ralphrapheals@gmail.com>
2024-02-15 20:09:37 +0000

    First Committtt

--- loop.js
+++ loop.js
@@ -1,1 +1,15 @@
-hi 
+const age = 18;
+
+//always use the 2 equal to instead of take
+// the 3 checks for both data datatype and equivalence
+/*
+if ( age === 18){
+    console.log("You can vote one person")
+} else if (age > 18){
+    console.log("You can vote two people")
+}else {
+    console.log("You cant vote")
+}
+*/
+
+const 
